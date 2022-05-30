8ee096409d58ec8a29aad2763d7f7f9badb7a9d9
Tyler Klier <tylerklier@gmail.com>
2022-05-30 13:35:46 -0400

    add initial sops commits

---
+++ flux/cluster/flux_install.tf
@@ -0,0 +1,117 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.10.0"
+    }
+    flux = {
+      source  = "fluxcd/flux"
+      version = ">= 0.0.13"
+    }
+  }
+}
+
+provider "flux" {}
+
+provider "kubectl" {}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+
+
+# Generate manifests
+data "flux_install" "main" {
+  target_path    = var.target_path
+  network_policy = false
+}
+
+resource "kubernetes_namespace" "flux_system" {
+  metadata {
+    name = "flux-system"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata[0].labels,
+    ]
+  }
+}
+
+# Split multi-doc YAML with
+# https://registry.terraform.io/providers/gavinbunney/kubectl/latest
+data "kubectl_file_documents" "apply" {
+  content = data.flux_install.main.content
+}
+
+# Convert documents list to include parsed yaml data
+locals {
+  apply = [ for v in data.kubectl_file_documents.apply.documents : {
+      data: yamldecode(v)
+      content: v
+    }
+  ]
+}
+
+# Apply manifests on the cluster
+resource "kubectl_manifest" "apply" {
+  for_each   = { for v in local.apply : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
+  depends_on = [kubernetes_namespace.flux_system]
+  yaml_body = each.value
+}
+
+
+
+
+
+
+
+# Generate manifests
+data "flux_sync" "main" {
+  target_path = var.target_path
+  url         = "https://github.com/${var.github_owner}/${var.repository_name}"
+}
+
+# Split multi-doc YAML with
+# https://registry.terraform.io/providers/gavinbunney/kubectl/latest
+data "kubectl_file_documents" "sync" {
+  content = data.flux_sync.main.content
+}
+
+# Convert documents list to include parsed yaml data
+locals {
+  sync = [ for v in data.kubectl_file_documents.sync.documents : {
+      data: yamldecode(v)
+      content: v
+    }
+  ]
+}
+
+# Apply manifests on the cluster
+resource "kubectl_manifest" "sync" {
+  for_each   = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
+  depends_on = [kubernetes_namespace.flux_system]
+  yaml_body = each.value
+}
+
+# Generate a Kubernetes secret with the Git credentials
+resource "kubernetes_secret" "main" {
+  depends_on = [kubectl_manifest.apply]
+
+  metadata {
+    name      = data.flux_sync.main.secret
+    namespace = data.flux_sync.main.namespace
+  }
+
+  data = {
+    username = "git"
+    password = var.github_token
+  }
+}
